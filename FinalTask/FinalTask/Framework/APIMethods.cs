8345bf0572e637c257d7bc393994b2a6f3bf8a3e
VOstrovskyiForte <volodymyr.ostrovskyi.forte@gmail.com>
2019-01-30 21:38:27 +0200

    created api methods

---
+++ FinalTask/FinalTask/Framework/APIMethods.cs
@@ -0,0 +1,33 @@
+﻿using RestSharp;
+using RestSharp.Serialization.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTask.Framework
+{
+    public class APIMethods
+    {
+
+
+        public IRestResponse SendRequest(Method requestMethod, string baseURL, string resource, object body = null, string contentType = "application/json")
+        {
+
+
+            IRestClient client = new RestClient(baseURL);
+            IRestRequest request = new RestRequest(resource, requestMethod);
+            if (body != null)
+                request.AddBody(body);
+            request.AddHeader("Content-Type", contentType);
+            return client.Execute(request);
+        }
+
+        public T DeserializeResponse<T>(IRestResponse response)
+        {
+            JsonDeserializer deserializer = new JsonDeserializer();
+            return deserializer.Deserialize<T>(response);
+        }
+    }
+}
