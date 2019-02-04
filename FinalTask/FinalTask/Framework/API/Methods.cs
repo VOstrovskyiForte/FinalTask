67643dd96da83fce46e80f8472cf96e965f691fc
VOstrovskyiForte <volodymyr.ostrovskyi.forte@gmail.com>
2019-02-04 19:45:17 +0200

    fixed posts tests

--- FinalTask/FinalTask/Framework/API/Methods.cs
+++ FinalTask/FinalTask/Framework/API/Methods.cs
@@ -11,16 +11,19 @@ namespace FinalTask.Framework.API
     public class Methods
     {
 
+        public static JsonSerializer serializer = new JsonSerializer();
 
-        public static IRestResponse SendRequest(Method requestMethod, string baseURL, string resource, object body = null, string contentType = "application/json")
+
+        public static IRestResponse SendRequest(Method requestMethod, string baseURL, string resource, object body = null, string contentType = "application/json;")
         {
 
 
             IRestClient client = new RestClient(baseURL);
             IRestRequest request = new RestRequest(resource, requestMethod);
             if (body != null)
-                request.AddBody(body);
+                request.AddBody(serializer.Serialize(body));
             request.AddHeader("Content-Type", contentType);
+            request.RequestFormat = DataFormat.Json;
             return client.Execute(request);
         }
 
